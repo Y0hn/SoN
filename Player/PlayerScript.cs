b700c587eb053b2fe63ef4a21e4acd616600a4cd
John <101512527+Y0hn@users.noreply.github.com>
2024-08-17 17:40:51 +0200

    2D transformation
    netCode multiplayer (local)
    2D scene

--- Player/PlayerScript.cs
+++ Player/PlayerScript.cs
@@ -1,30 +1,36 @@
-using UnityEngine;
 using UnityEngine.InputSystem;
-public class PlayerControler : MonoBehaviour
+using Unity.Netcode;
+using UnityEngine;
+public class PlayerControler : NetworkBehaviour
 {
-    [SerializeField]
-    Animator animator;
-    [SerializeField]
-    Rigidbody2D rb;
-    [SerializeField]
-    InputActionReference input_move;
-    [SerializeField]
-    InputActionReference input_fire;
+    [SerializeField] GameObject cam;
+    [SerializeField] Rigidbody2D rb;
+    [SerializeField] Animator animator;
+    [SerializeField] InputActionReference input_move;
+    [SerializeField] InputActionReference input_fire;
 
     public float speed = 1f;
-    private Vector2 moveDir;
-    private const float minC = 0.1f;
+    Vector2 moveDir;
+    const float minC = 0.1f;
 
     void Start()
     {
-        input_fire.action.performed += Fire;
+        if (IsOwner)
+        {
+            input_fire.action.performed += Fire;
+            cam.SetActive(true);
+        }
     }
     void Update()
     {
+        if (!IsOwner)
+            return;
         moveDir = input_move.action.ReadValue<Vector2>();
     }
     void FixedUpdate()
     {
+        if (!IsOwner)
+            return;
         if (moveDir.magnitude > minC)
         {
             if (!animator.GetBool("move"))
