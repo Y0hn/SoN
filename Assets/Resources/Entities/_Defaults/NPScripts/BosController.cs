16bba5e09ec88f8c6052381013a497197e9350cd
John <101512527+Y0hn@users.noreply.github.com>
2025-01-19 14:44:23 +0100

    boss {veles} base + some moving

---
+++ Assets/Resources/Entities/_Defaults/NPScripts/BosController.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class BosController : NPController
+{
+    /* ZDEDENE ATRIBUTY
+     *  [SF] protected EntityStats stats;
+     *  [SF] AIDestinationSetter destinationSetter;
+     *  [SF] AIPath path;
+     *  [SF] NPSensor sensor;
+     *  protected Vector2 moveDir;
+     *  protected bool attacking;
+     *  protected const float MIN_MOVE_TOL = 0.1f;
+     *  protected NextAction nextAction;
+     *  protected float nextDecisionTimer = 0f;
+     *  protected List<Transform> patrol = new();
+     *  protected bool selfTarget;
+     *
+     *  public bool ForceDecision       { get; protected set; }
+     *  protected Vector3 TargetPosition => sensor.ClosestTarget.position;    
+     *  *  *  *  *  *  *  *  *  *  *  *  *  *  */
+    protected new BosStats Stats => (BosStats)base.Stats;
+}
---
+++ Assets/Resources/Entities/_Defaults/NPScripts/BosStats.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+
+public class BosStats : NPStats
+{
+    /*  ZDEDENE ATRIBUTY
+     *      
+     *  [SF] protected TMP_Text nameTag;
+     *  [SF] protected Rase rase;
+     *  [SF] protected Slider hpBar;
+     *  [SF] protected GameObject body;
+     *  [SF] protected NetworkObject netObject;
+     *  [SF] protected Transform attackPoint;
+     *  [SF] protected SpriteRenderer weaponR, weaponL;
+     *  [SF] protected NetworkAnimator animator;
+     *  [SF] protected Rigidbody2D rb;
+     *  [SF] protected Behavior behavior = Behavior.Neutral;
+     *  [SF] protected NPSensor sensor;
+     *  [SF] protected Equipment[] setUpEquipment;
+     *  [SF] protected bool drawGizmo = false;
+     *
+     *  protected Defence defence;
+     *  protected const float timeToDespawn = 0f;
+     *  private bool clampedDMG = true;
+     *
+     *  [SF] protected NetworkVariable<int> maxHp = new();
+     *
+     *  protected NetworkVariable<int> hp = new();
+     *  protected NetworkVariable<Attack> attack = new ();
+     *  protected NetworkVariable<FixedString128Bytes> weapRef = new();
+     *  public NetworkVariable<bool> IsAlive = new(true);
+     *  public NetworkVariable<float> speed = new();
+     *  public NetworkVariable<byte> level = new(1);
+     *
+     *  public float HP                 { get { return (float)hp.Value/(float)maxHp.Value; } }
+     *  public NetworkObject NetObject  { get { return netObject; } }
+     *  public Rigidbody2D RigidBody2D  { get { return rb; } }
+     *  public AITarget TargetTeam      { get { return aiTeam; } }
+     *  public Animator Animator        { get { return animator.Animator; } }
+     *  public bool AttackBoth          { get { return attack.Value.bothHanded; } }
+     *  public bool Armed               { get { return equipment[(int)Equipment.Slot.WeaponL] != "" || "" !=  equipment[(int)Equipment.Slot.WeaponR]; } }
+     *
+     *
+     *  protected const float RANGED_ATTACK_INACURRACY = 0.4f; // symbolizuje percento casu utoku kedy nedostava polohu ciela [+inacuracy => -presnost]
+     *  protected const float ATTACK_DISTANCE_PERCENTAGE = 0.3f;
+     *  public override Quaternion Rotation     { get => body.transform.rotation; }
+     *  public float AttackDistance             
+     *  public override Attack Attack
+     *  public Defence.Class DC     { get; protected set; }
+     *  public Weapon.Class WC      { get; protected set; }
+     *  public Behavior Behave      { get { return behavior; } protected set => behavior = value; }
+     *  public bool AboutToFire    => aToFire <= Time.time;
+     *  public float NextAttackTime=> atTime;
+     *  public Action OnHit;
+     *  protected float aToFire;
+     *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  */
+}
