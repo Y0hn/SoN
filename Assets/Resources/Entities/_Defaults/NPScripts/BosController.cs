320c539ae11c81d784c24f894837bd28177a7226
y0hn <janikanco@gmail.com>
2025-01-21 07:27:47 +0000

    [Not Tested] Boss controller

--- Assets/Resources/Entities/_Defaults/NPScripts/BosController.cs
+++ Assets/Resources/Entities/_Defaults/NPScripts/BosController.cs
@@ -19,4 +19,68 @@ public class BosController : NPController
      *  protected Vector3 TargetPosition => sensor.ClosestTarget.position;    
      *  *  *  *  *  *  *  *  *  *  *  *  *  *  */
     protected new BosStats Stats => (BosStats)base.Stats;
+
+    protected override void Update()
+    {
+        base.Update();
+
+        DecideNextMove();
+    }
+
+    protected virtual void DecideNextMove()
+    {
+        float nextChange = 1f;
+        bool inRange = false, gotInRange = false ;
+
+        switch (Stats.WC)
+        {
+            default:
+                break;
+        }
+        switch (Stats.DC)
+        {
+            default:
+                break;
+        }
+        switch (Stats.Behave)
+        {
+            case NPStats.Behavior.Scared:       DecideByTreshhold(1, inRange, gotInRange, out nextChange);      break;
+            case NPStats.Behavior.Berserk:      DecideByTreshhold(0, inRange, gotInRange, out nextChange);      break;
+            case NPStats.Behavior.Neutral:      DecideByTreshhold(0.5f, inRange, gotInRange, out nextChange);   break;
+            case NPStats.Behavior.Agressive:    DecideByTreshhold(0.2f, inRange, gotInRange, out nextChange);   break;
+            case NPStats.Behavior.Defesive:     DecideByTreshhold(0.7f, inRange, gotInRange, out nextChange);   break;
+        }
+        if (ForceDecision)
+            ForceDecision = false;
+        nextDecisionTimer = Time.time + nextChange;
+    }
+    void DecideByTreshhold(float tHP, bool inRange, bool gotRange, out float tDecay)
+    {
+        float hp = stats.HP;
+        if      (hp < tHP && inRange)
+        {
+            nextAction = NextAction.RunFromTarget;
+            tDecay = 3;
+        }
+        else if (gotRange && (!inRange || inRange && tHP < hp))
+        {
+            nextAction = NextAction.AttackTarget;
+            tDecay = 0.5f;
+        }
+        else if (!gotRange && tHP < hp)
+        {
+            nextAction = NextAction.RunToTarget;
+            tDecay = 0.2f;
+        }
+        else if (patrol.Count > 0)
+        {
+            nextAction = NextAction.GoToTarget;
+            tDecay = 1;
+        }
+        else
+        {
+            nextAction = NextAction.StayOnPlace;
+            tDecay = 3;
+        }
+    }
 }
--- Assets/Resources/Entities/_Defaults/NPScripts/NPController.cs
+++ Assets/Resources/Entities/_Defaults/NPScripts/NPController.cs
@@ -104,61 +104,6 @@ public class NPController : EntityController
             stats.Animator.SetFloat("vertical", viewDir.y);
         }
     }/*
-    protected virtual void DecideNextMove()
-    {
-        float nextChange = 1f;
-        bool inRange = false, gotInRange = false ;
-
-        switch (((NPStats)stats).WC)
-        {
-            default:
-                break;
-        }
-        switch (((NPStats)stats).DC)
-        {
-            default:
-                break;
-        }
-        switch (((NPStats)stats).Behave)
-        {
-            case NPStats.Behavior.Scared:       DecideByTreshhold(1, inRange, gotInRange, out nextChange);      break;
-            case NPStats.Behavior.Berserk:      DecideByTreshhold(0, inRange, gotInRange, out nextChange);      break;
-            case NPStats.Behavior.Neutral:      DecideByTreshhold(0.5f, inRange, gotInRange, out nextChange);   break;
-            case NPStats.Behavior.Agressive:    DecideByTreshhold(0.2f, inRange, gotInRange, out nextChange);   break;
-            case NPStats.Behavior.Defesive:     DecideByTreshhold(0.7f, inRange, gotInRange, out nextChange);   break;
-        }
-        if (ForceDecision)
-            ForceDecision = false;
-        nextDecisionTimer = Time.time + nextChange;
-    }
-    void DecideByTreshhold(float tHP, bool inRange, bool gotRange, out float tDecay)
-    {
-        float hp = stats.HP;
-        if      (hp < tHP && inRange)
-        {
-            nextAction = NextAction.RunFromTarget;
-            tDecay = 3;
-        }
-        else if (gotRange && (!inRange || inRange && tHP < hp))
-        {
-            nextAction = NextAction.AttackTarget;
-            tDecay = 0.5f;
-        }
-        else if (!gotRange && tHP < hp)
-        {
-            nextAction = NextAction.RunToTarget;
-            tDecay = 0.2f;
-        }
-        else if (patrol.Count > 0)
-        {
-            nextAction = NextAction.GoToTarget;
-            tDecay = 1;
-        }
-        else
-        {
-            nextAction = NextAction.StayOnPlace;
-            tDecay = 3;
-        }
-    }*/
+    */
     protected enum NextAction { GoToTarget, RunToTarget, AttackTarget, RunFromTarget, StayOnPlace, None }
 }
